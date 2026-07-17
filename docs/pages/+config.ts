c46ef3382d0a743003cdda00b3b8c4bec715c0a8
Christian Landgren <christian@landgren.nu>
2026-07-17 11:07:39 +0200

    chore: remove accidentally committed docs files

--- docs/pages/+config.ts
+++
@@ -1,9 +0,0 @@
-import vikeReact from 'vike-react/config'
-import type { Config } from 'vike/types'
-
-export default {
-  extends: vikeReact,
-  title: 'r8s Documentation',
-  lang: 'en',
-  prerender: true,
-} satisfies Config
